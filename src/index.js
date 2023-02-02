37ade1c6b8c4dbae60b1c13925f12344adefc26e
Abhishek Singh <84718593+abhsin96@users.noreply.github.com>
2023-02-02 16:50:29 +0530

    class created

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
+class API {
+  #secure;
+  constructor(url) {
+    this.url = url;
+    this.method = "GET";
+    this.#secure = false;
+  }
 
+  isSecure() {
+    return this.#secure;
+  }
 
-module.exports = { API }
+  updateUrl(newUrl) {
+    this.url = newUrl;
+    this.#secure = newUrl.startsWith("https");
+  }
+}
+
+const s = new API("http://api.com/api/hello");
+console.log(s.isSecure()); // false
+
+s.updateUrl("https://api.com/api/hello");
+console.log(s.isSecure()); // true
+
+console.log(s.url); // https://api. com/api/hello
+
+console.log(s.method); // GET
+
+console.log(s.secure); // undefined (because private field)
+
+module.exports = { API };
