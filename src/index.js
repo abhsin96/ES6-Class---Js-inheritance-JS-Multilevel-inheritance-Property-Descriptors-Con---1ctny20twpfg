a2aec456fd6144eb49a228e948c26064a0738505
Abhishek Singh <84718593+abhsin96@users.noreply.github.com>
2023-02-02 16:54:56 +0530

    #secure update

--- src/index.js
+++ src/index.js
@@ -12,14 +12,14 @@ class API {
 
   updateUrl(newUrl) {
     this.url = newUrl;
-    this.#secure = newUrl.startsWith("https");
+    this.#secure = newUrl.startsWith("https://");
   }
 }
 
 const s = new API("http://api.com/api/hello");
 console.log(s.isSecure()); // false
 
-s.updateUrl("https://api.com/api/hello");
+s.updateUrl("https://");
 console.log(s.isSecure()); // true
 
 console.log(s.url); // https://api. com/api/hello
