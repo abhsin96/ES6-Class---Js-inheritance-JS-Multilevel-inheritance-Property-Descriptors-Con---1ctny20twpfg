3db5e1cf1a05dcc6d9541307d1711f1c90ed1f19
Abhishek Singh <84718593+abhsin96@users.noreply.github.com>
2023-02-02 16:55:45 +0530

    example deleted

--- src/index.js
+++ src/index.js
@@ -16,16 +16,4 @@ class API {
   }
 }
 
-const s = new API("http://api.com/api/hello");
-console.log(s.isSecure()); // false
-
-s.updateUrl("https://");
-console.log(s.isSecure()); // true
-
-console.log(s.url); // https://api. com/api/hello
-
-console.log(s.method); // GET
-
-console.log(s.secure); // undefined (because private field)
-
 module.exports = { API };
